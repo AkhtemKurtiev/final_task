194efbe69c6faf0d6ae8a7ed97928c9ae0a3ae5b
Akhtem Kurtiev <kurtievahtem20@gmail.com>
2024-08-26 10:48:48 +0300

    add user register and login router

---
+++ src/api/user/v1/routers/users.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from src.shemas.user import UserCreate, UserResponse
+from src.models.user import User
+from src.auth.utils import hash_password, validate_password, encode_jwt
+from src.database.db import get_async_session
+
+router = APIRouter(
+    prefix='/users',
+    tags=['User']
+)
+
+
+@router.post('/register/', response_model=UserResponse)
+async def register(user: UserCreate, db: AsyncSession = Depends(get_async_session)):
+    result = await db.execute(select(User).filter(User.email == user.email))
+    db_user = result.scalars().first()
+    if db_user:
+        raise HTTPException(status_code=400, detail='Email already registered')
+
+    hashed_password = hash_password(user.password)
+
+    new_user = User(
+        email=user.email,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        username=user.username,
+        hashed_password=hashed_password,
+        company_id=user.company_id,
+        position_id=user.position_id
+    )
+    db.add(new_user)
+    await db.commit()
+    return new_user
+
+
+@router.post('/login')
+async def login(user: UserCreate, db: AsyncSession = Depends(get_async_session)):
+    result = await db.execute(select(User).filter(User.email == user.email))
+    db_user = result.scalars().first()
+    if not db_user:
+        raise HTTPException(status_code=400, detail='Invalid data')
+
+    if not validate_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail='Invalid data')
+
+    access_token = encode_jwt({'sub': db_user.email})
+    return {'access_token': access_token, 'token_type': 'bearer'}
--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 
-# from src.api.v1.routers import v1_spimex_router
+from src.api.user.v1.routers import v1_user_router
 
 
 async def startup():
@@ -37,3 +37,5 @@ app = FastAPI(
     title='Radius',
     lifespan=lifespan
 )
+
+app.include_router(v1_user_router)
