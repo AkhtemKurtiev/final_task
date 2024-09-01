e79ee09db584dd40b9eef49f3a6f05144e771c76
Akhtem Kurtiev <kurtievahtem20@gmail.com>
2024-09-01 12:46:59 +0300

    add: repository for models

--- src/repositories/company.py
+++ src/repositories/company.py
@@ -4,3 +4,10 @@ from src.utils.repository import SqlAlchemyRepository
 
 class CompanyRepository(SqlAlchemyRepository):
     model = Company
+
+    async def add_company(self, name):
+        new = self.model(name=name)
+        self.session.add(new)
+        await self.session.commit()
+        await self.session.refresh(new)
+        return new
--- src/repositories/position.py
+++ src/repositories/position.py
@@ -4,3 +4,12 @@ from src.utils.repository import SqlAlchemyRepository
 
 class PositionRepository(SqlAlchemyRepository):
     model = Position
+
+    async def add_position(self, name, department):
+        new_position = self.model(name=name, department=department)
+        self.session.add(new_position)
+        await self.session.commit()
+        await self.session.refresh(new_position)
+
+    async def get_position(self, position_id):
+        return await self.session.get(self.model, position_id)
