a617098da24b1e04e11c35c29d7efcc695d76ea7
Akhtem Kurtiev <kurtievahtem20@gmail.com>
2024-09-03 09:10:20 +0300

    add: test registration

--- src/config.py
+++ src/config.py
@@ -12,6 +12,14 @@ DB_PORT = os.environ.get('DB_PORT')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 
+load_dotenv('.test.env', encoding='latin-1')
+
+DB_NAME_TEST = os.environ.get('DB_NAME_TEST')
+DB_HOST_TEST = os.environ.get('DB_HOST_TEST')
+DB_PORT_TEST = os.environ.get('DB_PORT_TEST')
+DB_USER_TEST = os.environ.get('DB_USER_TEST')
+DB_PASS_TEST = os.environ.get('DB_PASS_TEST')
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
